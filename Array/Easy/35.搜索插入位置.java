ecf6ad8fcdb392aa222057d9dbede695b2692b52
chiperman <chipermanju@gmail.com>
2023-06-30 14:38:32 +0800

    create 35.搜索插入位置.java

---
+++ Array/Easy/35.搜索插入位置.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=java
+ *
+ * [35] 搜索插入位置
+ */
+
+// @lc code=start
+class Solution {
+    // 暴力解法
+    public int searchInsert1(int[] nums, int target) {
+        // 目标值在数组所有元素之前
+        if (target <= nums[0]) {
+            return 0;
+        }
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            // 目标值等于数组中某一个元素  
+            if (target == nums[i]) {
+                return i;
+            }
+            // 目标值插入数组中的位置 
+            if (nums[i] <= target && target <= nums[i + 1]) {
+                return i + 1;
+            }
+        }
+        // 目标值在数组所有元素之后的情况 
+        return nums.length;
+    }
+
+    // 二分法
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            // 防止溢出 等同于(left + right)/2
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else if (nums[middle] >= target) {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+
+}
+// @lc code=end
--- linkedList/Easy/83.删除排序链表中的重复元素.java
+++ linkedList/Easy/83.删除排序链表中的重复元素.java
@@ -12,9 +12,23 @@
 /**
  * 输入：head = [1,1,2]
  * 输出：[1,2]
+ * 
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
  */
+
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
+    /**
+     * 比较当前节点与下一个节点的值是否相等来判断是否存在重复元素。如果相等，将慢指针的next指向快指针，跳过重复元素；如果不相等，将慢指针向后移动。
+     * 1. 首先，检查链表是否为空，如果为空，则直接返回空。
+     * 2. 初始化快慢指针，初始时都指向头节点。
+     * 3. 使用快指针遍历链表，直到快指针到达链表末尾（即**`fast.next == null`**）。
+     * 4. 比较快指针和慢指针所指节点的值是否相等。如果相等，说明存在重复元素，将慢指针的**`next`**指向快指针，
+     *    跳过重复元素；如果不相等，将慢指针向后移动，并将慢指针的**`next`**指向快指针。
+     * 5. 遍历完成后，断开与后面重复元素的连接，将慢指针的**`next`**设置为**`null`**。
+     * 6. 返回头节点。
+     */
+    public ListNode deleteDuplicates1(ListNode head) {
         if (head == null) {
             return null;
         }
@@ -32,6 +46,21 @@ class Solution {
 
         // 断开与后面重复元素的连接
         slow.next = null;
+        return head;
+    }
+
+    /**
+     * 只需要遍历一次即可，如果 cur.val == cur.next.val（当前节点的值等于当前节点下一个节点的值），就让当前节点指向下下一个节点
+     */
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
         return head;
     }
 }
