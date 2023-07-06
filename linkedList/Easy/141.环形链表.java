67461a07207c227dfde2e623ba679625c6602c5f
chiperman <chipermanju@gmail.com>
2023-07-06 14:03:45 +0800

    Create and add
    create 141.环形链表 & add 21.合并两个有序链表

---
+++ linkedList/Easy/141.环形链表.java
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    // 快慢指针法
+    public boolean hasCycle2(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+    
+    // 哈希表
+    public boolean hasCycle2(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+
+        while (cur != null) {
+            cur = cur.next;
+            if (set.contains(cur)) {
+                return true;
+            }
+            set.add(cur);
+            cur = cur.next;
+        }
+        return false;
+    }
+}
+// @lc code=end
--- linkedList/Easy/21.合并两个有序链表.java
+++ linkedList/Easy/21.合并两个有序链表.java
@@ -16,14 +16,7 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
-        if (list1 == null) {
-            return list2;
-        } else if (list2 == null) {
-            return list1;
-        }
-
+    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
         ListNode dummy = new ListNode();
         ListNode cur = dummy;
 
@@ -42,8 +35,25 @@ class Solution {
         } else {
             cur.next = list1;
         }
-        
+
         return dummy.next;
+    }
+    
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val <= list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
     }
 }
 // @lc code=end
