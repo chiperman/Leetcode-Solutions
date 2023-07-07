cc21c7456c3fe08d6734f3b9a0b6fc1899c1091e
chiperman <chipermanju@gmail.com>
2023-07-07 10:23:12 +0800

    create 86.分隔链表.java

---
+++ linkedList/Medium/86.分隔链表.java
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode.cn id=86 lang=java
+ *
+ * [86] 分隔链表
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode list1 = new ListNode();
+        ListNode cur1 = list1;
+        ListNode list2 = new ListNode();
+        ListNode cur2 = list2;
+
+        while (head != null) {
+            if (head.val < x) {
+                cur1.next = head;
+                cur1 = cur1.next;
+            } else {
+                cur2.next = head;
+                cur2 = cur2.next;
+            }
+            head = head.next;
+        }
+
+        cur1.next = list2.next;
+        cur2.next = null;
+        return list1.next;
+
+    }
+}
+// @lc code=end
