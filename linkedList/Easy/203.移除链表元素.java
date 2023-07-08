92e632ccb92ed01896227409330c83be300cd7a8
chiperman <chipermanju@gmail.com>
2023-07-08 10:45:27 +0800

    Create 203.移除链表元素.java

---
+++ linkedList/Easy/203.移除链表元素.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=203 lang=java
+ *
+ * [203] 移除链表元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = head;
+        ListNode pre = dummy;
+        dummy.next = head;
+
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
