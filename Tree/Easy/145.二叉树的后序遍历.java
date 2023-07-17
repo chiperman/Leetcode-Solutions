95aa290b63e495cb501f40ee5346acdfad88286e
chiperman <chipermanju@gmail.com>
2023-07-17 10:50:52 +0800

    Create 145.二叉树的后序遍历.java

---
+++ Tree/Easy/145.二叉树的后序遍历.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=java
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    void postorder(TreeNode root, List list) {
+        if (root == null) {
+            return;
+        }
+
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+}
+// @lc code=end
