036a8b07b20c73e448a6b59b0866ffb5a781a70f
chiperman <chipermanju@gmail.com>
2023-07-18 11:33:32 +0800

    Create 226.翻转二叉树.java

---
+++ Tree/Easy/226.翻转二叉树.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+}
+
+class Solution3 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+// @lc code=end
