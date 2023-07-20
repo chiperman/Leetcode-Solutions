5d12552683ead66701d6a3e699692da533e29467
chiperman <chipermanju@gmail.com>
2023-07-20 09:28:59 +0800

    Update 226.翻转二叉树.java

--- Tree/Easy/226.翻转二叉树.java
+++ Tree/Easy/226.翻转二叉树.java
@@ -37,7 +37,7 @@ class Solution {
     }
 }
 
-class Solution3 {
+class Solution2 {
     public TreeNode invertTree(TreeNode root) {
         if (root == null) {
             return null;
@@ -50,6 +50,32 @@ class Solution3 {
         invertTree(root.left);
         invertTree(root.right);
 
+        return root;
+    }
+}
+
+class Solution3 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        // 先处理根节点
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            TreeNode tmp = cur.left;
+            cur.left = cur.right;
+            cur.right = tmp;
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
         return root;
     }
 }
