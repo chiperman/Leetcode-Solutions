2b8db4849c6321a3aeb364c4ee602814ceab1355
chiperman <chipermanju@gmail.com>
2023-07-22 11:43:49 +0800

    Create 103.二叉树的锯齿形层序遍历.java

---
+++ Tree/Medium/103.二叉树的锯齿形层序遍历.java
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=103 lang=java
+ *
+ * [103] 二叉树的锯齿形层序遍历
+ */
+
+// @lc code=start
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new LinkedList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        queue.offer(root);
+
+        // flags = true，表明奇数行，从左向右遍历
+        // flags = false，表面偶数行，从右向左遍历
+        Boolean flags = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            Deque<Integer> temp = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (flags == true) {
+                    temp.offerLast(node.val);
+                } else {
+                    temp.offerFirst(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            flags = !flags;
+            result.add(new LinkedList<Integer>(temp));
+        }
+        return result;
+    }
+}
+// @lc code=end
