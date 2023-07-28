f9118e36d70f8030be05e94dd57daeb0947196fb
chiperman <chipermanju@gmail.com>
2023-07-28 10:17:49 +0800

    Create 707.设计链表.java

---
+++ linkedList/Medium/707.设计链表.java
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=707 lang=java
+ *
+ * [707] 设计链表
+ */
+
+// @lc code=start
+class MyLinkedList {
+
+    //size存储链表元素的个数
+    int size;
+    //虚拟头结点
+    ListNode head;
+
+    public MyLinkedList() {
+        size = 0;
+        head = new ListNode(0);
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            return -1;
+        }
+
+        ListNode cur = head;
+
+        for (int i = 0; i <= index; i++) {
+            cur = cur.next;
+        }
+        return cur.val;
+    }
+
+    public void addAtHead(int val) {
+        addAtIndex(0, val);
+
+    }
+
+    public void addAtTail(int val) {
+        addAtIndex(size, val);
+    }
+
+    public void addAtIndex(int index, int val) {
+        if (index > size) {
+            return;
+        }
+        if (index < 0) {
+            index = 0;
+        }
+        size++;
+        ListNode cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
+
+        ListNode addNode = new ListNode(val);
+        addNode.next = cur.next;
+        cur.next = addNode;
+
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index < 0 || index >= size) {
+            return;
+        }
+
+        size--;
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        ListNode pred = head;
+        for (int i = 0; i < index; i++) {
+            pred = pred.next;
+        }
+        pred.next = pred.next.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+// @lc code=end
